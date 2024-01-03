19f2d7599c792e8742cf88cb238d3c77b44176bc
judedaniel2023 <daniel.jude.87536@unn.edu.ng>
2024-01-04 00:35:04 +0100

    This is the first commit

---
+++ jblog_main/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+from blogs.models import Category
+
+def home(request):
+    categories = Category.objects.all()
+    context = {
+        'categories': categories,
+    }
+    return render(request, 'home.html', context=context)
